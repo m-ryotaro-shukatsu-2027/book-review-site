5640499e1a64d721a069c22a3a60aa8f980b806f
m-ryotaro-shukatsu-2027 <m.ryotaro.shukatsu.2027@gmail.com>
2025-09-05 10:43:44 +0900

    名前を変更

---
+++ m6_create_table.php
@@ -0,0 +1,18 @@
+<?php
+$dsn = 'mysql:dbname=tb270439db;host=localhost';
+$user = 'tb-270439';
+$password = 'CZtyxCGmfM';
+$pdo = new PDO($dsn, $user, $password, array(PDO::ATTR_ERRMODE => PDO::ERRMODE_WARNING));
+
+$sql = "CREATE TABLE IF NOT EXISTS mission6_1"
+        ." ("
+        . "id INT AUTO_INCREMENT PRIMARY KEY,"
+        . "name CHAR(32),"
+        . "comment TEXT,"
+        . "created_at DATETIME,"
+        . "password VARCHAR(255),"
+        . "image VARCHAR(255)"
+        .");";
+ 
+$stmt = $pdo->query($sql);
+?>
